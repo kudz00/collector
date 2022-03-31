8d6e0d828ffdb7edd54688169053dfaee91874a8
JoukoVirtanen <jouko@stackrox.com>
2022-03-30 21:25:48 -0700

    Refactored part of integration-test-data/20-gather-perf-data.sh

--- .circleci/integration-test-data/20-gather-perf-data.sh
+++ .circleci/integration-test-data/20-gather-perf-data.sh
@@ -7,16 +7,9 @@ for i in "${WORKSPACE_ROOT}"/*perf.json; do
     cat "$i" >> "${WORKSPACE_ROOT}"/all-perf.json
 done
 
-## TODO put into script
-#jq '. | select(.Metrics.hackbench_avg_time != null) | {kernel: .VmConfig, collection_method: .CollectionMethod, (.TestName): .Metrics.hackbench_avg_time } ' < all-perf.json | jq -rs  ' group_by(.kernel) | .[] | group_by(.collection_method) | .[] | add | [.kernel, .collection_method, .baseline_benchmark, .collector_benchmark ] | @csv' > "${WORKSPACE_ROOT}/benchmark.csv"
-
-# I could not come up with a better variable name than temp
-temp=$(jq '. | select(.Metrics.hackbench_avg_time != null) | {kernel: .VmConfig, collection_method: .CollectionMethod, (.TestName): .Metrics.hackbench_avg_time } ' < "${WORKSPACE_ROOT}"/all-perf.json)
-temp=$(echo "$temp" | jq -rs  ' group_by(.kernel) | .[] | group_by(.collection_method) | .[] ')
-echo "$temp" | jq -r ' add | [.kernel, .collection_method, .baseline_benchmark, .collector_benchmark ] | @csv' > "${WORKSPACE_ROOT}/benchmark.csv"
-
-##cat "${WORKSPACE_ROOT}/benchmark.csv" | sort | awk -v FS="," 'BEGIN{print "|Kernel|Method|Without Collector Time (secs)|With Collector Time (secs)|";print "|---|---|---|---|"}{printf "|%s|%s|%s|%s|%s",$1,$2,$3,$4,ORS}' > "${WORKSPACE_ROOT}/benchmark.md"
-#
+jq '. | select(.Metrics.hackbench_avg_time != null) | {kernel: .VmConfig, collection_method: .CollectionMethod, (.TestName): .Metrics.hackbench_avg_time } ' < "${WORKSPACE_ROOT}"/all-perf.json \
+        | jq -rs  ' group_by(.kernel) | .[] | group_by(.collection_method) | .[] | add | [.kernel, .collection_method, .baseline_benchmark, .collector_benchmark ] | @csv' \
+        > "${WORKSPACE_ROOT}/benchmark.csv"
 
 echo "|Kernel|Method|Without Collector Time (secs)|With Collector Time (secs)|" > "${WORKSPACE_ROOT}/benchmark.md"
 echo "|---|---|---|---|" >> "${WORKSPACE_ROOT}/benchmark.md"
